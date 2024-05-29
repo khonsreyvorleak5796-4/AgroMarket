9d40a47fa9619d96eab185fc1f95c4dd23244a8f
limsopeak16 <sopheaklim70@gmail.com>
2024-05-29 15:43:34 +0700

    develop home page

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+
+
 const apiToken = '2e79eefd74136248b06f27da2fc503f057a6db43d166d27ac3cc8a20c3da67a1fc3de621e710b23a89ce1ac95fe44807daf44b7dc774e182c458a42b2acb7868fc3ddc6424f22a89d149c807b74d617d920a34b38a40743940e85c2a598e6b30a7117ecb70c2ecd77b54f6cfabbbed40e8acab56b91f545d309808338a948d18';
-const apiUrl = 'https://clever-eggs-512f1b05ad.strapiapp.com/api/products?populate=*';
+const apiUrl = 'https://clever-eggs-512f1b05ad.strapiapp.com/api/products?';
 
 function fetchAndDisplayProducts(url, containerId) {
   fetch(url, {
@@ -9,7 +11,6 @@ function fetchAndDisplayProducts(url, containerId) {
   })
   .then(response => response.json())
   .then(data => {
-    console.log('Complete response data:', data); // Log the complete response data
     if (!data || !data.data || data.data.length === 0) {
       console.log('No data found.');
     } else {
@@ -25,8 +26,9 @@ function displayProducts(products, containerId) {
 
   products.forEach(product => {
     const attributes = product.attributes;
-    const imageUrl = attributes.images.data.attributes.url;
+    const imageUrl = attributes.images?.data?.attributes?.url || 'default-image-url.jpg';
     const productLink = `/product/${product.id}`;
+    
     const productCard = `
       <div class="col-md-4">
         <div class="card position-relative">
@@ -47,13 +49,13 @@ function displayProducts(products, containerId) {
 }
 
 // URLs for each category
-const fruitsUrl = `${apiUrl}&category_id=1&pagination[pageSize]=6`;
-const vegetablesUrl = `${apiUrl}&category_id=2&pagination[pageSize]=6`;
-const tractorsUrl = `${apiUrl}&category_id=3&pagination[pageSize]=6`;
-const fertilizersUrl = `${apiUrl}&category_id=4&pagination[pageSize]=6`;
+const fruitsUrl = `${apiUrl}filters[category_ids][CategoryID][$eq]=1&populate=*&pagination[limit]=6`;
+const vegetablesUrl = `${apiUrl}filters[category_ids][CategoryID][$eq]=2&populate=*&pagination[limit]=6`;
+const tractorsUrl = `${apiUrl}filters[category_ids][CategoryID][$eq]=3&populate=*&pagination[limit]=6`;
+const fertilizersUrl = `${apiUrl}filters[category_ids][CategoryID][$eq]=4&populate=*&pagination[limit]=6`;
 
 // Fetch and display products for each category
 fetchAndDisplayProducts(fruitsUrl, 'fruits-container');
-fetchAndDisplayProducts(vegetablesUrl, 'vegetables-container');
-fetchAndDisplayProducts(tractorsUrl, 'tractors-container');
-fetchAndDisplayProducts(fertilizersUrl, 'fertilizers-container');
+ fetchAndDisplayProducts(vegetablesUrl, 'vegetables-container');
+ fetchAndDisplayProducts(tractorsUrl, 'tractors-container');
+ fetchAndDisplayProducts(fertilizersUrl, 'fertilizers-container');
